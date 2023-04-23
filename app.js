08e50507635592f5b9a64c3d48ad76c1804551bc
Faran Ahmad <ahmad.faran332@gmail.com>
2023-04-23 16:53:27 +0200

    simple popup window and basic form

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
+let popup = document.getElementById("contact-form");
 
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+function openContactForm() {
+  popup.classList.add("open-contact__form--container");
+}
+
+function closeContactForm() {
+  popup.classList.remove("open-contact__form--container");
+}
