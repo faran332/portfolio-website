09a00b5d6a0bfe3444db1591e46db01130311b28
Faran Ahmad <ahmad.faran332@gmail.com>
2023-04-27 18:34:58 +0200

    can send email via contact form

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
+const serviceID = "service_4pu43vt";
+const templateID = "template_c5nb9sd";
 let popup = document.getElementById("contact-form");
 
 menu.addEventListener("click", function () {
@@ -13,4 +15,23 @@ function openContactForm() {
 
 function closeContactForm() {
   popup.classList.remove("open-contact__form--container");
+}
+
+function sendMessage() {
+  var templateParams = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message-box").value,
+  };
+
+  emailjs
+    .send(serviceID, templateID, templateParams)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message-box").value = "";
+      console.log(res);
+      alert("Your message has been sent successfully. Thank you.");
+    })
+    .catch((err) => console.log(err));
 }
